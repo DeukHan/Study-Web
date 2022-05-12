4fc491fd1def1e5b320fb3d85e276870e15a4df4
DeukHan <104338516+DeukHan@users.noreply.github.com>
2022-05-12 22:43:56 +0900

    improve: Component Event

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import { buildQueries } from '@testing-library/react';
     constructor(props){
       super(props);
       this.state={
-        mode:'welcome',
+        mode:'read',
+        selected_content_id:2,
         Subject:{title:'WEB',sub:'World Wid Web!'},
         welcome:{title:'Welcome', desc:'Hello, React!!'},
         contents:[
@@ -28,27 +29,34 @@ import { buildQueries } from '@testing-library/react';
         _title  = this.state.welcome.title;
         _desc  = this.state.welcome.desc;
       }else if (this.state.mode === 'read'){
-        _title = this.state.contents[0].title;
-        _desc = this.state.contents[0].desc;
+        var i =0;
+        while(i < this.state.contents.length){
+          var data = this.state.contents[i];
+          if(data.id === this.state.selected_content_id) {
+            _title = data.title;
+            _desc = data.desc;
+            break;
+          }
+          i = i + 1;
+        }
       }
       return (
         <div className="App">
-          {/*<Subject 
-          title={this.state.Subject.title} 
-          sub={this.state.Subject.sub}>
-          </Subject>*/}
-          <header>
-            <h1><a href="/"onClick={function(e){
-              console.log(e);
-              e.preventDefault()
-              this.state.mode = 'welcome';
+          <Subject 
+            title={this.state.Subject.title} 
+            sub={this.state.Subject.sub}
+            onChangePage={function(){
+                this.setState({mode:'welcome'});
+            }.bind(this)}
+          >
+          </Subject>
+          <TOC onChangePage={function(id){
               this.setState({
-                mode:'welcome'
+                mode:'read',
+                selected_content_id:Number(id)
               });
-            }.bind(this)}>{this.state.Subject.title}</a></h1>
-            {this.state.Subject.sub}
-          </header>
-          <TOC data={this.state.contents}></TOC>
+          }.bind(this)} 
+          data={this.state.contents}></TOC>
           <Content title={_title} desc={_desc}></Content>
         </div>
       );
--- src/Components/TOC.js
+++ src/Components/TOC.js
@@ -6,8 +6,17 @@ class TOC extends Component {
       var data = this.props.data;
       var i = 0;
       while (i < data.length) {
-    //아래부분에서 key를 입력하지 않으면 js Error가 발생할 수 있다.
-          list.push(<li key={data[i].id}><a href={"/content/" + data[i].id}>{data[i].title}</a></li>);
+          list.push(
+        <li key={data[i].id}>
+            <a 
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+            >{data[i].title}</a>
+        </li>);
           i = i + 1;
       }
       return (
